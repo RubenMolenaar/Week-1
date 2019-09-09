e061b8ff4271d5919be0f95e3ef61f487f4514bd
Ruben Molenaar <rubenmolenaar2002@gmail.com>
2019-09-09 13:40:50 +0200

    Pop-up laten weergeven en antwoorden laten weergeven

---
+++ b1w2l1.js
@@ -0,0 +1,10 @@
+alert('U moet nu 3 vragen gaan beantwoorden')
+var Voornaam = prompt('Voer je naam in');
+var Achternaam = prompt('Voer je achternaam in');
+var Leeftijd = prompt('Voer je leeftijd in');
+document.write(Voornaam)
+document.write('</br>')
+document.write(Achternaam)
+document.write('</br>')
+document.write(Leeftijd)
+document.write('</br>')
