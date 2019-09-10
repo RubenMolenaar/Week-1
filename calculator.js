fc529e5c575af92905db323354e4cf80a3a5763a
Ruben Molenaar <rubenmolenaar2002@gmail.com>
2019-09-10 11:25:43 +0200

    calculator gemaakt

---
+++ calculator.js
@@ -0,0 +1,15 @@
+var a = parseInt(prompt("voer je eerste nummer in"));
+var x = prompt("voer hier je muliplyer in");
+var b = parseInt(prompt("voer je tweede numme in"));
+if (x == "*") {
+	alert(a * b)
+}
+else if(x == "/") {
+	alert(a / b)
+}
+else if(x == "+") {
+	alert(a + b)
+}
+else if(x == "-") {
+	alert(a - b)
+}
